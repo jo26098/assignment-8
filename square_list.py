cfdb1b5b0ef314f50d2dbdbdc3ab07acaaeaa6c7
jo26098 <osbornj2@oregonstate.edu>
2026-08-04 15:19:36 -0700

    Create square_list.py

---
+++ square_list.py
@@ -0,0 +1,10 @@
+def square_list():
+    """
+    Outputs the product of the two arguments.
+        
+    Arguments: 
+        nums (list of ints): 
+        
+    Returns:
+        squared_nums (list of ints): 
+    """
