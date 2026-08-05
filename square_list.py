d9121c92cf47f88ed67993343608b9be8a8e2c99
jo26098 <osbornj2@oregonstate.edu>
2026-08-04 19:15:38 -0700

    Update square_list.py

--- square_list.py
+++ square_list.py
@@ -1,10 +1,17 @@
-def square_list():
+def square_list(nums):
     """
     Outputs the product of the two arguments.
         
     Arguments: 
-        nums (list of ints): 
+        nums (list of ints): The list of integers that the function will modify.
         
     Returns:
-        squared_nums (list of ints): 
+        none
     """
+    for i in range(len(nums)):
+        nums[i] = nums[i] ** 2
+
+
+#list_of_nums = [0, 1, 2, 3, 4, 5, 6]
+#square_list(list_of_nums)
+#print(list_of_nums)
